d357e55e19128fdb9d710639db340b40e2da1238
Asha Kripalani <119749754+ashakrip@users.noreply.github.com>
2023-03-23 18:43:44 +0530

    Update brown_dwarfs_stars_data_scraping.py

--- brown_dwarfs_stars_data_scraping.py
+++ brown_dwarfs_stars_data_scraping.py
@@ -49,8 +49,3 @@ for i in range(1,len(temp_list)):
     Radius.append(temp_list[i][8])
 
 # Convert to CSV
-headers = ['Star_name','Distance','Mass','Radius']  
-df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,)),columns=['Star_name','Distance','Mass','Radius'])
-print(df2)
-
-df2.to_csv('dwarf_stars.csv', index=True, index_label="id")
